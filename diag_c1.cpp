aeff2999f6842a1f624efaf3f82510378c21589f
PhantomMajor <udayaditya1998@gmail.com>
2018-12-01 16:43:15 +0530

    The final version of diagonal has been uploaded

--- diag_c1.cpp
+++ diag_c1.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 int main ()
 {
     // image of dimensions 500 x 500
-    Mat img(500, 500, CV_8UC3, schar(0));
+    Mat img(100, 100, CV_8UC3, schar(0));
     cvNamedWindow("win", WINDOW_NORMAL);
 
     int rows = img.rows;
@@ -15,13 +15,13 @@ int main ()
 
     for (int i= 0; i< rows; i++)
     {
-        // vary i(row index) from 0 to rows(500)
+        // vary i(row index) from 0 to rows(100)
         for (int j= 0; j< cols-i; j++)
         {
             // vary j(col index) from 0 to (diagonal element)
             img.at<Vec3b>(i, j) = {0, 0, 255};// red color
         }
-        for  (int j=cols-i-1; j>=0; j--)
+        for  (int j=cols-i; j< cols; j++)
         {
             // vary j(col index) from last col to diagonal element
             img.at<Vec3b>(i, j) = {0, 255, 255};// yellow color
